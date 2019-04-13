c170f0f0e4b1a5db867d067618706e097d411745
oneilld136 <oneilld136@gmail.com>
2019-04-13 00:26:38 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,9 +4,9 @@ require 'rspec'
 def fizzBuzz(number)
   if number % 3 == 0 && number % 5 == 0
     puts "FizzBuzz"
-  elsif number % 3 == 0
+  elsif number % 3 #== 0
     puts "Fizz"
-  elsif number % 5 == 0
+  elsif number % 5 #== 0
     puts "Buzz"
   else
     puts number
