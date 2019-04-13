67e5d9a9999a9fd4e11ad6fb1d86b40844d7c9e8
oneilld136 <oneilld136@gmail.com>
2019-04-13 00:56:28 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,5 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-require 'rspec'
 def fizzbuzz(number)
   if number % 3 == 0 && number % 5 == 0
     puts "FizzBuzz"
