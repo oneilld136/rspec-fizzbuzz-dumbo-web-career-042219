911c298dd3d96858ffabfb406c409b684b61b2ca
oneilld136 <oneilld136@gmail.com>
2019-04-13 01:24:43 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,7 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(number)
   if number % 3 == 0 && number % 5 == 0
-    puts "FizzBuzz"
+    #puts "FizzBuzz"
   elsif number % 3 == 0
     puts "Fizz"
   elsif number % 5 == 0
